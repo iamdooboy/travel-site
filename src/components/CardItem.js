4ed68b1a8ed4c99bf5b022c0cf4b62e1838e068f
Duy Le <asianinvasion.le@gmail.com>
2021-01-29 10:51:38 -0500

    card

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
-function CardItem() {
+function CardItem(props) {
     return (
         <>
-            <li className="cards__item">
-                <Link className="cards_item_link">
-                    <figure className="cards__item_pic-wrap">
-                        <img src="/images/img-1.jpg" alt="Travel Image" className="cards__item__img" />
+            <li className='cards__item'>
+                <Link className="cards__item__link" to={props.path}>
+                    <figure className="cards__item__pic-wrap" data-category={props.label}>
+                        <img src={props.src} alt="Travel Image" className="cards__item__img" />
                     </figure>
-                    <div className="cards__item__info">
-                        <h5 className="cards__item-text">
-
-                        </h5>
+                    <div className='cards__item__info'>
+                        <h5 className="cards__item-text">{props.text}</h5>
                     </div>
                 </Link>
             </li>
